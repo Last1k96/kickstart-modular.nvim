29e1e28eb8e72a838abbefb5ed36efa89bd89c0d
Maxim Kurin <maxim.kurin@intel.com>
2025-12-22 13:42:22 +0000

    enable osc52

--- lua/options.lua
+++ lua/options.lua
@@ -44,11 +44,11 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
---  vim.opt.clipboard
-vim.opt.clipboard = 'unnamedplus'
+-- Force the OSC52 provider in case tmux masks auto-detection
+vim.g.clipboard = 'osc52'
+
+-- Use the system clipboard by default for yanks/puts ("+")
+vim.opt.clipboard:append 'unnamedplus'
 
 -- Enable break indent
 vim.opt.breakindent = true
